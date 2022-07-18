ca115a86fc54b345c1277866d43a48af594e792d
lucky-gru <hiroshi.tan57@gmail.com>
2022-07-18 03:48:33 -0500

    use uuid as a key instead of postcode

--- src/pages/AddressBook/index.tsx
+++ src/pages/AddressBook/index.tsx
@@ -1,43 +1,50 @@
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import './style.css'
 import AddressItem from '../../components/AddressItem'
 import AddressForm from '../../components/App/AddressForm'
 import Button from '../../components/Button'
 import { Address } from '../../types'
 
+type AddressListItem = Address & { id: string };
+
 function AddressBook() {
-  const [list, setList] = useState<Address[]>([
+  const [list, setList] = useState<AddressListItem[]>([
     {
+      id: uuidv4(),
       line1: 'New York street',
       postcode: '2342',
       town: 'New York',
       country: 'US',
     },
   ])
-  const [selected, setSelected] = useState<string>('');
+  const [selected, setSelected] = useState<string>('')
 
   const add = (newAddress: Address) => {
-    setList(prev => ([
+    setList((prev) => [
       ...prev,
-      newAddress
-    ]))
+      {
+        ...newAddress,
+        id: uuidv4(),
+      },
+    ])
   }
 
   return (
     <div className="container address-book">
       <div className="address-book__list">
-        {list.map((item: Address) => (
+        {list.map((item: AddressListItem) => (
           <AddressItem
-            key={item.postcode+item.line1+item.town+item.country}
+            key={item.id}
             {...item}
             onClick={() => {
-              if (item.postcode === selected) {
+              if (item.id === selected) {
                 setSelected('')
               } else {
-                setSelected(item.postcode)
+                setSelected(item.id)
               }
             }}
-            selected={item.postcode === selected ? true : false}
+            selected={item.id === selected ? true : false}
           />
         ))}
         <div className="addres-book__button-wrap">
@@ -45,7 +52,7 @@ function AddressBook() {
         </div>
       </div>
       <div className="address-book__form">
-        <AddressForm submit={add}/>
+        <AddressForm submit={add} />
       </div>
     </div>
   )
